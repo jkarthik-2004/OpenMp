dc32c838d1dc35e2f28ebedf8bf08ae04cb9d8fd
jkarthik-2004 <99941226+jkarthik-2004@users.noreply.github.com>
2023-08-09 20:41:05 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,8 @@
 #include<omp.h>
 #include<stdio.h>
-
 int main() {
-
 #pragma omp parallel
     {
- 
         printf("Hello World... from thread = %d\n",
                omp_get_thread_num());
     }

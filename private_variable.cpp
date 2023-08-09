4b2e966cf57daf93fbb4a34e0a3777f22ec49694
jkarthik-2004 <99941226+jkarthik-2004@users.noreply.github.com>
2023-08-09 20:47:32 +0530

    Update private_variable.cpp

--- private_variable.cpp
+++ private_variable.cpp
@@ -5,6 +5,7 @@ int main() {
 int nthreads,tid;
 #pragma omp parallel private(nthreads,tid)
 {
+    int id;
     omp_set_num_threads(100);
     printf("The number of threads allocated are %d\n",omp_get_num_threads());
     tid = omp_get_thread_num();
@@ -12,3 +13,8 @@ int nthreads,tid;
 }
 return 0;
 }
+
+// Here we are setting threads inside the parallel region
+// Placing the omp_set_num_threads call inside the parallel region could potentially 
+// lead to confusion and unexpected results in more complex programs.
+// also variable " id " used only inside the parallel region (private variable) due to block scope.

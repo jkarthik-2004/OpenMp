bde17799b9465937a0323b3b2bfb1743e5a2ac27
jkarthik-2004 <99941226+jkarthik-2004@users.noreply.github.com>
2023-08-09 20:44:00 +0530

    Update and rename private_variables.cpp to set_threads.cpp

--- set_threads.cpp
+++ set_threads.cpp
@@ -12,3 +12,6 @@ printf("The number of threads allocated are %d\n",omp_get_num_threads());
 }
 return 0;
 }
+
+// Here we are setting number of threads outside the parallel region it is considered to be the best practice
+// to ensure consistent behavior across different OpenMP implementations and scenarios.
